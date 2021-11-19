0301c664a20c65cc8fc5830e1cf18a1e95969dbb
colepacak <cole.w.pacak@gmail.com>
2021-11-19 10:01:03 -0600

    more renaming and update bash script for package management

--- circle-create-new-version.sh
+++ circle-create-new-version.sh
@@ -5,28 +5,13 @@
 #   Create a git commit containing these actions
 #
 
-# Get particular image being built from the tag name
-# Expected tag format is: image_full-vendor-path_application-name_version
-# e.g. php-7.1-fpm_drupal-8_12
+CHARTNAME="drupal"
 
-IFS="_" read -ra TAG <<< "$CIRCLE_TAG"
-
-index=0
-for val in "${TAG[@]}"; do
-    if [ $index -eq 0 ]; then
-        CHARTNAME=$val
-    fi
-    if [ $index -eq 1 ]; then
-        VERSION=$val
-    fi
-    ((index++))
-done
-
-echo "Circle tag is $CIRCLE_TAG, chartname: $CHARTNAME, version: $VERSION"
+echo "chartname: $CHARTNAME, version: $CIRCLE_TAG"
 
 # Always add a newline in case the chart author doesn't terminate the file with one.
 echo "" >> ./$CHARTNAME/Chart.yaml
-echo "version: $VERSION" >> ./$CHARTNAME/Chart.yaml
+echo "version: $CIRCLE_TAG" >> ./$CHARTNAME/Chart.yaml
 
 cat ./$CHARTNAME/Chart.yaml
 
@@ -38,7 +23,7 @@ git checkout .
 git fetch origin gh-pages
 git checkout gh-pages
 
-helm repo index . --url https://favish.github.com/helm-charts
+helm repo index . --url https://favish.github.com/helm/$CHARTNAME
 
 git config --global user.name "favish-ci"
 git config --global user.email "dev@favish.com"
